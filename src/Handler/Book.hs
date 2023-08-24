19d7003ea302b674bfe945882d87a6c3d91bf1d6
ciukstar <ciukstar@yahoo.com>
2023-08-25 02:08:27 +0300

    Add serices to choose from for booking

--- src/Handler/Book.hs
+++ src/Handler/Book.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Handler.Book (getBookR) where
 
@@ -8,18 +9,40 @@ import Yesod.Core.Widget (setTitleI)
 import Yesod.Auth (maybeAuth, Route (LoginR, LogoutR))
 import Settings (widgetFile)
 
+import Yesod.Persist.Core (runDB)
 import Database.Persist ( Entity(Entity) )
+import Database.Persist.Sql ( fromSqlKey )
+
+import Database.Esqueleto.Experimental
+    ( select, from, table, where_, innerJoin, on
+    , (^.), (==.), (:&)((:&))
+    , orderBy, asc
+    )
 
 import Foundation
     ( Handler
     , Route (AuthR, PhotoPlaceholderR, AccountPhotoR)
-    , AppMessage (MsgBook, MsgPhoto, MsgLogout)
+    , AppMessage
+      ( MsgBook, MsgPhoto, MsgLogout, MsgChooseServicesToBook
+      )
+    )
+    
+import Model
+    ( Service(Service)
+    , Pricelist (Pricelist)
+    , EntityField (ServiceId, PricelistService, ServicePublished, ServiceName)
     )
 
 
 getBookR :: Handler Html
 getBookR = do
     muid <- maybeAuth
+    offers <- runDB $ select $ do
+        x :& p <- from $ table @Service `innerJoin` table @Pricelist
+            `on` (\(x :& p) -> x ^. ServiceId ==. p ^. PricelistService)
+        where_ $ x ^. ServicePublished
+        orderBy [asc (x ^. ServiceName)]
+        return (x,p)
     setUltDestCurrent
     defaultLayout $ do
         setTitleI MsgBook
