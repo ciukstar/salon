f599aeacd7a9ee05a9b03244208e40039c885b73
ciukstar <ciukstar@yahoo.com>
2024-01-30 17:08:27 +0300

    Change docs title

--- src/Handler/Resources.hs
+++ src/Handler/Resources.hs
@@ -26,11 +26,11 @@ import Foundation
       , BusinessHoursR, BrandR, AdmInvoicesR
       )
     , AppMessage
-      ( MsgDocumentation, MsgPhoto, MsgNavigationMenu, MsgLogin, MsgUserProfile
+      ( MsgAppDocumentation, MsgPhoto, MsgNavigationMenu, MsgLogin, MsgUserProfile
       , MsgErdDiagram, MsgBookingStateDiagram, MsgAppointmentStateDiagram
       , MsgBasicEntities, MsgBusiness, MsgUser, MsgPaymentGateway, MsgOnlineMaps
       , MsgAppName, MsgOverview, MsgStaff, MsgService, MsgOffer, MsgInvoice
-      , MsgEmail, MsgSearchEngineOptimization
+      , MsgEmail, MsgSearchEngineOptimization, MsgAppDescription
       , MsgDoc001, MsgDoc002, MsgDoc003, MsgDoc004, MsgDoc005, MsgDoc0061
       , MsgDoc0062, MsgDoc0063, MsgDoc0064, MsgDoc0065, MsgDoc0066, MsgDoc0067
       , MsgDoc0068, MsgDoc007, MsgDoc008, MsgDoc009, MsgDoc010, MsgDoc011
@@ -65,5 +65,5 @@ getDocsR = do
     business <- runDB $ selectOne $ from $ table @Business
 
     defaultLayout $ do
-        setTitleI MsgDocumentation
+        setTitleI MsgAppDocumentation
         $(widgetFile "resources/docs")
